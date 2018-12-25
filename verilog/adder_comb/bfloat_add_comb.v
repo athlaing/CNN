65a41c0683dcb9bb9bdf0bb29660f355b342e516
samtruong <mstruong@ucdavis.edu>
2018-12-25 12:59:04 -0800

    ADD: compiled adder_comb

--- verilog/adder_comb/bfloat_add_comb.v
+++ verilog/adder_comb/bfloat_add_comb.v
@@ -3,33 +3,38 @@ module bfloat_add_comb(clk, a, b, out);
   input [15:0] a;
   input [15:0] b;
   output reg [15:0] out;
+  reg [15:0] a_r, b_r;
   reg [15:0] out_c;
   wire a_s, b_s;
   reg [7:0] a_m, b_m;
   reg [7:0] a_m_next, b_m_next;
   wire [9:0] a_e, b_e;
   reg [9:0] holder_e;
+  reg [8:0] result;
+  reg [6:0] out_m;
+  reg [6:0] out_e;
+  wire [9:0] diff_e;
 
   assign a_s = a_r[15];
   assign b_s = b_r[15];
   assign a_e = {2'b00, a_r[14:7]} + 10'b1110000001;
   assign b_e = {2'b00, b_r[14:7]} + 10'b1110000001;
-  assign = diff_e = a_e + ~(b_e) + + 10'b0000_0000_01;
-  assign = neg_diff_e = ~(diff_e) + 10'b0000_0000_01;
+  assign diff_e = a_e + ~(b_e) + + 10'b0000_0000_01;
+  assign neg_diff_e = ~(diff_e) + 10'b0000_0000_01;
 
   always  @(*) begin
     // if not denormal, add implicit 1
     if(a_e != 10'b1110000001) begin
-      a_m = {1'b1, a_r[6:0]}
+      a_m = {1'b1, a_r[6:0]};
     end
     else begin
-      a_m = {1'b0, a_r[6:0]}
+      a_m = {1'b0, a_r[6:0]};
     end
     if(b_e != 10'b1110000001) begin
       b_m = {1'b1, b_r[6:0]};
     end
     else begin
-      b_m = {1'b0, b_r[6:0]}
+      b_m = {1'b0, b_r[6:0]};
     end
     // if a_exp is greater, shift b_exp
     if(diff_e[9] == 1'b0) begin
@@ -71,15 +76,15 @@ module bfloat_add_comb(clk, a, b, out);
     end
     if((result[8:7] == 2'b11) || (result[8:7] == 2'b10)) begin
       out_m = result[8:2];
-      out_e = holder[7:0] + 8'b0000_0010;
+      out_e = holder_e[7:0] + 8'b0000_0010;
     end
-    else if(result_c[8:7] == 2'b01) begin
+    else if(result[8:7] == 2'b01) begin
       out_m = result[7:1];
-      out_e = holder[7:0] + 8'b0000_0001;
+      out_e = holder_e[7:0] + 8'b0000_0001;
     end
-    else if(result_c[8:7] == 2'b00) begin
+    else if(result[8:7] == 2'b00) begin
       out_m = result[6:0];
-      out_e = holder[7:0];
+      out_e = holder_e[7:0];
     end
     // if infinity or NaN, return 16 ones
     if((a_e == 10'b0010000000) || (b_e == 10'b0010000000)) begin
@@ -116,3 +121,4 @@ module bfloat_add_comb(clk, a, b, out);
     a_r <= a;
     b_r <= b;
   end
+endmodule
