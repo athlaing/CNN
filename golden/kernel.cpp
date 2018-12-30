8714b453b85739ca5f0a6d5a0717e6d82da0f63b
Arthur Hlaing <athlaing@ucdavis.edu>
2018-12-29 19:41:36 -0800

    ADD: cpp kernel

---
+++ golden/kernel.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+float kernel(int kernel_size, float* input, float* weight) {
+  int array_size = (kernel_size * kernel_size - 1) + (kernel_size * kernel_size);
+  float* arr = new float[array_size]();
+  for(int i = 0; i < kernel_size * kernel_size; i++) {
+    arr[i] = input[i] * weight[i];
+  }
+  int s_add = ceil(log2(kernel_size * kernel_size)); // number of addition stages
+  bool even = false;
+  if ((kernel_size * kernel_size) % 2) {
+    even = false;
+  }
+  else {
+    even = true;
+  }
+  int num_add = (kernel_size * kernel_size) / 2;
+  int read_index = 0;
+  int write_index = kernel_size * kernel_size;
+  for(int stage = 0; stage < s_add; stage++) {
+    for(int i = 0; i < num_add; i++) {
+      arr[write_index] = arr[read_index] + arr[read_index + 1];
+      write_index++;
+      read_index += 2;
+    }
+    if(stage == 0) {
+      read_index += 1;
+    }
+    num_add /= 2;
+  }
+  if (!even) {
+    arr[array_size - 1] = arr[array_size - 2] + arr[kernel_size * kernel_size - 1];
+  }
+  // if you dont want to print, you can comment out everything below until return
+  int newline = kernel_size * kernel_size;
+  int count = 0;
+  for(int i = 0; i < (kernel_size * kernel_size - 1) + (kernel_size * kernel_size); i++) {
+    if(count == newline) {
+      cout << endl;
+      newline /= 2;
+      count = 0;
+    }
+    cout << arr[i] << " ";
+    count++;
+  }
+  delete [] arr;
+  return 1;
+}
+
+
+int main() {
+  float input[9];
+  float weight[9];
+  for(int i = 0; i < 9; i++) {
+    input[i] = i;
+    weight[i] = i;
+  }
+  kernel(3, input, weight);
+  return 0;
+}
