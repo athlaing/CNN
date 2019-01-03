0c7e1c2a765228423e911ed8c457994ce5238e46
wbenli <wbenli21@gmail.com>
2019-01-02 23:58:55 -0800

    UPDATE: -w, --write flag to write output to file.

--- golden/testgen/checker.py
+++ golden/testgen/checker.py
@@ -1,7 +1,16 @@
 import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Use -w or --write to generate a diff file.')
+parser.add_argument("-w","--write", help="Enable write to file. Default is false", default=False)
+args = vars(parser.parse_args())
+write2file = args["write"] == "True"
 
 fvt = open("mult_out.csv", "r")
 fref = open("mult_ref.csv", "r")
+if write2file : 
+	fdiff = open("diff.txt", 'w')
+
 lvt = fvt.readlines()
 lref = fref.readlines()
 error = 0
@@ -10,7 +19,6 @@ errorContent = []
 if fvt == None or fref == None:
 	print("FATA: ERROR: No require files are in the directory.")
 	sys.exit()
-
 if len(lvt) != len(lref):
 	print("ERROR: Mult_out and mult_ref aren't the same length.")
 else:
@@ -21,10 +29,16 @@ else:
 				": " + lvt[i] + ' '*22 + lref[i])
 
 print("Total Number of mismatches: " + str(error))
+if write2file:
+	fdiff.write("Total Number of mismatches: " + str(error) + '\n')
 for e in errorContent:
 	print(e)
+	if write2file:
+		fdiff.write(e)
 
 fvt.close()
 fref.close()
+if write2file:
+	fdiff.close()
 
 
